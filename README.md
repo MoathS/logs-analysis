56f3ce0f45e1bf13af347b192c54c3dbda070e84
Muath <muath@hotmail.es>
2018-11-18 01:30:02 +0300

    inital commit

---
+++ README.md
@@ -0,0 +1,26 @@
+#Logs Analysis Project
+
+This is ***logs analysis project*** for _full stack developer course_
+
+## How to run this project
+by using `python 3.7`, `Vagrant 2.2.0` and `PostgreSQL`.
+
+## How to read this Project
+it's been writen to show up three output from newsdata.sql:
+
+### What are the most popular three articles of all time?
+
+### Who are the most popular article authors of all time?
+### On which days did more than 1% of requests lead to errors?
+
+_two easy one and another took my whole day :)._
+
+## This project tested with pycodestyle
+to make sure of the style and to fullfill the requirments this project used pycodestyle testing tool and it return only E501 alert due to a long query statement.
+
+
+## **NOTE:***
+this repository only contain three files:
+### Source code written by python3 named 'logs-analysis.py'.
+### an example of the output 'output.txt' file.
+### README.md file.
---
+++ logs-analysis.py
@@ -0,0 +1,33 @@
+
+import psycopg2
+
+DBNAME = "news"
+db = psycopg2.connect(database=DBNAME)
+c = db.cursor()
+print('What are the most popular three articles of all time?')
+c.execute("select articles.title, count(*) from articles, log where log.path like concat('%', articles.slug) and log.status = '200 OK' group by articles.title order by count desc limit 3")
+db.commit()
+printQ = c.fetchall()
+for x in range(0, 3):
+    print('"', printQ[x][0], '"', ' -- ', printQ[x][1], 'views \n')
+
+print("\nWho are the most popular article authors of all time?")
+
+c.execute("select authors.name, count(*) from authors,articles,log where log.path like concat('%', articles.slug) and log.status = '200 OK' and authors.id = articles.author group by authors.name order by count desc limit 4")
+db.commit()
+printQ = c.fetchall()
+for x in range(0, 4):
+    print(printQ[x][0], ' -- ', printQ[x][1], 'views \n')
+
+print("\nOn which days did more than 1% of requests lead to errors?")
+
+c.execute("select to_char(l.time, 'Month DD,YYYY'), ((r1.sta) *100/(r2.nu)) as per from log l, (select to_char(log.time, 'Month DD,YYYY') as r1da, cast(count(status) as float) as sta from log where status!='200 OK' group by to_char(log.time, 'Month DD,YYYY')) as r1, (select to_char(log.time, 'Month DD,YYYY') as r2da, cast(count(*) as float) as nu from log  group by to_char(log.time, 'Month DD,YYYY')) as r2 where (r1.sta*100/r2.nu) > 1 and r1.r1da = to_char(l.time, 'Month DD,YYYY') and r2.r2da= to_char(l.time, 'Month DD,YYYY') GROUP by to_char(l.time, 'Month DD,YYYY') , per order by per desc;")
+
+db.commit()
+printQ = c.fetchall()
+le = len(printQ)
+for x in range(0, le):
+    print(printQ[x][0], ' -- ', printQ[x][1], '% \n')
+
+
+db.close()
