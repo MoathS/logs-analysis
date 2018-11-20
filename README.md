1897cb19a94bf60f6ab6c26bae188585182fd61a
Muath <muath@hotmail.es>
2018-11-20 19:50:11 +0300

    Modifing inital

--- README.md
+++ README.md
@@ -1,26 +1,29 @@
 #Logs Analysis Project
 
-This is ***logs analysis project*** for _full stack developer course_
-
-## How to run this project
-by using `python 3.7`, `Vagrant 2.2.0` and `PostgreSQL`.
-
-## How to read this Project
-it's been writen to show up three output from newsdata.sql:
-
+  This is project is to provide an answers to three questions:
 ### What are the most popular three articles of all time?
-
 ### Who are the most popular article authors of all time?
 ### On which days did more than 1% of requests lead to errors?
 
-_two easy one and another took my whole day :)._
+---
+## Tools needed to run this project
+  [python 3.7](https://www.python.org/downloads/), [Vagrant 2.2.0](https://www.vagrantup.com/), [VirtualBox5.1.38](https://www.virtualbox.org/).
 
-## This project tested with pycodestyle
-to make sure of the style and to fullfill the requirments this project used pycodestyle testing tool and it return only E501 alert due to a long query statement.
+### How to setup & run vagrant
+  To setup the vagrant clone this repository: [fullstack-nanodegree-vm](https://github.com/udacity/fullstack-nanodegree-vm)
+  then using the terminal inside the vagrant subdirectory, run the command `vagrant up` to intall the Linux VM.
+  then run `vagrant ssh` to access the LinuxVM.
+  to access vagrant subdirectory after Runing `vagrant ssh` change directory to `cd /vagrant`.
+
+---
+## Runing the Datebase
+  First you need to download the database for this project [newsdata.db](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip) unzip this file then Put it into the vagrant directory.
+  We are using `PostgreSQL` in this project, To load the data run `psql -d news -f newsdata.sql` inside vagrant VM.
 
 
-## **NOTE:***
-this repository only contain three files:
+---
+## **NOTE:**
 ### Source code written by python3 named 'logs-analysis.py'.
 ### an example of the output 'output.txt' file.
 ### README.md file.
+you need to download the database [newsdata.db](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip).
--- logs-analysis.py
+++ logs-analysis.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 
 import psycopg2
 
@@ -5,7 +6,11 @@ DBNAME = "news"
 db = psycopg2.connect(database=DBNAME)
 c = db.cursor()
 print('What are the most popular three articles of all time?')
-c.execute("select articles.title, count(*) from articles, log where log.path like concat('%', articles.slug) and log.status = '200 OK' group by articles.title order by count desc limit 3")
+c.execute("""select articles.title,count(*)
+from articles, log
+where log.path like concat('/article/', articles.slug)
+and log.status = '200 OK'
+group by articles.title order by count desc limit 3""")
 db.commit()
 printQ = c.fetchall()
 for x in range(0, 3):
@@ -13,7 +18,11 @@ for x in range(0, 3):
 
 print("\nWho are the most popular article authors of all time?")
 
-c.execute("select authors.name, count(*) from authors,articles,log where log.path like concat('%', articles.slug) and log.status = '200 OK' and authors.id = articles.author group by authors.name order by count desc limit 4")
+c.execute("""select authors.name, count(*)
+from authors,articles,log
+where log.path like concat('/article/', articles.slug)
+and log.status = '200 OK' and authors.id = articles.author
+group by authors.name order by count desc limit 4""")
 db.commit()
 printQ = c.fetchall()
 for x in range(0, 4):
@@ -21,7 +30,21 @@ for x in range(0, 4):
 
 print("\nOn which days did more than 1% of requests lead to errors?")
 
-c.execute("select to_char(l.time, 'Month DD,YYYY'), ((r1.sta) *100/(r2.nu)) as per from log l, (select to_char(log.time, 'Month DD,YYYY') as r1da, cast(count(status) as float) as sta from log where status!='200 OK' group by to_char(log.time, 'Month DD,YYYY')) as r1, (select to_char(log.time, 'Month DD,YYYY') as r2da, cast(count(*) as float) as nu from log  group by to_char(log.time, 'Month DD,YYYY')) as r2 where (r1.sta*100/r2.nu) > 1 and r1.r1da = to_char(l.time, 'Month DD,YYYY') and r2.r2da= to_char(l.time, 'Month DD,YYYY') GROUP by to_char(l.time, 'Month DD,YYYY') , per order by per desc;")
+c.execute("""
+select to_char(l.time, 'Month DD,YYYY'),
+ROUND(((r1.sta) *100/(r2.nu)), 2) as per
+from log l,
+(select to_char(log.time, 'Month DD,YYYY') as r1da,
+cast(count(status) as DECIMAL) as sta
+from log where status!='200 OK'
+group by to_char(log.time, 'Month DD,YYYY')) as r1,
+(select to_char(log.time, 'Month DD,YYYY') as r2da,
+cast(count(*) as DECIMAL) as nu
+from log  group by to_char(log.time, 'Month DD,YYYY')) as r2
+where (r1.sta*100/r2.nu) > 1 and r1.r1da = to_char(l.time, 'Month DD,YYYY')
+and r2.r2da= to_char(l.time, 'Month DD,YYYY')
+GROUP by to_char(l.time, 'Month DD,YYYY') , per
+order by per desc;""")
 
 db.commit()
 printQ = c.fetchall()
